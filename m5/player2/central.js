ee5d5a97527ebeda4ca3876101cc61b2f6b6aa43
Andrew Schechter <schechtera8620@gmail.com>
2022-12-06 18:15:39 -0800

    Add files via upload

---
+++ m5/player2/central.js
@@ -0,0 +1,79 @@
+// based on the example on https://www.npmjs.com/package/@abandonware/noble
+
+const noble = require('@abandonware/noble');
+
+const uuid_service = "1102"
+const uuid_values = ['2101', '2102', '2103'];
+
+let sensorValueX = NaN
+let sensorValueY = NaN
+let sensorValueZ = NaN
+
+noble.on('stateChange', async (state) => {
+    if (state === 'poweredOn') {
+        console.log("start scanning")
+        await noble.startScanningAsync([uuid_service], false);
+    }
+});
+
+noble.on('discover', async (peripheral) => {
+    await noble.stopScanningAsync();
+    await peripheral.connectAsync();
+    const {
+        characteristics
+    } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([uuid_service], uuid_values);
+    readData(characteristics)
+});
+
+//
+// read data periodically
+//
+let readData = async (characteristics) => {
+    const xvalue = (await characteristics[0].readAsync())
+    const yvalue = (await characteristics[1].readAsync())
+    const zvalue = (await characteristics[2].readAsync())
+
+    sensorValueX = xvalue.readFloatLE(0);
+    sensorValueY = yvalue.readFloatLE(0);
+    sensorValueZ = zvalue.readFloatLE(0);
+
+    console.log(sensorValueX + ','+ sensorValueY + ',' + sensorValueZ);
+
+    // read data again in t milliseconds
+    setTimeout(() => {
+        readData(characteristics)
+    }, 10);
+}
+
+//
+// hosting a web-based front-end and respond requests with sensor data
+// based on example code on https://expressjs.com/
+//
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+// Idiomatic expression in express to route and respond to a client request
+app.get('/', (req, res) => {   
+    res.sendFile('index.html', {root: __dirname});      
+                                                        
+});
+
+app.post('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    res.end(JSON.stringify({
+        sensorValueX: sensorValueX,
+        sensorValueY: sensorValueY,
+        sensorValueZ: sensorValueZ
+    }))
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+  
---
+++ m5/pong.js
@@ -0,0 +1,303 @@
+// select canvas element
+const canvas = document.getElementById("pong");
+
+// getContext of canvas = methods and properties to draw and do a lot of thing to the canvas
+const ctx = canvas.getContext('2d');
+
+// load sounds
+let hit = new Audio();
+let wall = new Audio();
+let player1Score = new Audio();
+let player2Score = new Audio();
+
+hit.src = "sounds/hit.mp3";
+wall.src = "sounds/wall.mp3";
+player2Score.src = "sounds/comScore.mp3";
+player1Score.src = "sounds/userScore.mp3";
+
+// Ball object
+const ball = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 10,
+    velocityX : 5,
+    velocityY : 5,
+    speed : 7,
+    color : "WHITE"
+}
+
+// player1 Paddle
+const player1 = {
+    x : 0, // left side of canvas
+    y : (canvas.height - 100)/2, // -100 the height of paddle
+    width : 10,
+    height : 100,
+    score : 0,
+    color : "WHITE"
+}
+
+
+/* COM Paddle
+const com = {
+    x : canvas.width - 10, // - width of paddle
+    y : (canvas.height - 100)/2, // -100 the height of paddle
+    width : 10,
+    height : 100,
+    score : 0,
+    color : "WHITE"
+}*/
+
+const player2 = {
+    x : canvas.width - 10, // - width of paddle
+    y : (canvas.height - 100)/2, // -100 the height of paddle
+    width : 10,
+    height : 100,
+    score : 0,
+    color : "WHITE"
+}
+
+
+// NET
+const net = {
+    x : (canvas.width - 2)/2,
+    y : 0,
+    height : 10,
+    width : 2,
+    color : "WHITE"
+}
+
+// draw a rectangle, will be used to draw paddles
+function drawRect(x, y, w, h, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
+// draw circle, will be used to draw the ball
+function drawArc(x, y, r, color){
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x,y,r,0,Math.PI*2,true);
+    ctx.closePath();
+    ctx.fill();
+}
+
+// listening to the mouse
+/*canvas.addEventListener("mousemove", getMousePos);
+
+function getMousePos(evt){
+    let rect = canvas.getBoundingClientRect();
+    
+    // update the user's paddle position
+    user.y = evt.clientY - rect.top - user.height/2;
+    console.log(evt.clientY);
+}*/
+
+// listening to the imu
+function updatePlayerPosition(){
+
+    const SERVERADDRESS1 = "http://127.0.0.1:3000";
+    const SERVERADDRESS2 = "http://127.0.0.1:3001";
+    var data = "";
+
+	// use XML HTTP request to grab data from the server (central)
+	let xmlhttp1 = new XMLHttpRequest();
+    let xmlhttp2 = new XMLHttpRequest();
+	xmlhttp1.onreadystatechange = function () {
+		if (xmlhttp1.readyState == 4 && xmlhttp1.status == 200) {
+			let responseText = xmlhttp.responseText;
+			let sensorValueX = parseFloat(JSON.parse(responseText).sensorValueX)
+			let sensorValueY = parseFloat(JSON.parse(responseText).sensorValueY)
+			let sensorValueZ = parseFloat(JSON.parse(responseText).sensorValueZ)
+
+			// calculate pitch 
+            let pitch = 1 * 180 * Math.atan2(sensorValueX, Math.sqrt(sensorValueY*sensorValueY + sensorValueZ*sensorValueZ))/Math.PI;
+            
+            console.log(player1.y);
+
+            // make the user paddle exist between 350 and -50
+            let newUserY = player1.y + 0.25*pitch;
+            if (newUserY >= 350) {
+                newUserY = 350;
+            }
+            else if (newUserY <= -50) {
+                newUserY = -50;
+            }
+            player1.y = newUserY;
+            
+		}
+	}
+    xmlhttp2.onreadystatechange = function () {
+		if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
+			let responseText = xmlhttp2.responseText;
+			let sensorValueX = parseFloat(JSON.parse(responseText).sensorValueX)
+			let sensorValueY = parseFloat(JSON.parse(responseText).sensorValueY)
+			let sensorValueZ = parseFloat(JSON.parse(responseText).sensorValueZ)
+
+			// calculate pitch 
+            let pitch = 1 * 180 * Math.atan2(sensorValueX, Math.sqrt(sensorValueY*sensorValueY + sensorValueZ*sensorValueZ))/Math.PI;
+            
+            console.log(player2.y);
+
+            // make the user paddle exist between 350 and -50
+            let newUserY = player2.y + 0.25*pitch;
+            if (newUserY >= 350) {
+                newUserY = 350;
+            }
+            else if (newUserY <= -50) {
+                newUserY = -50;
+            }
+            player2.y = newUserY;
+            
+		}
+	}
+
+	// periodically grab data
+	let getData = () => {
+		xmlhttp1.open("POST", SERVERADDRESS1, true);
+		xmlhttp1.setRequestHeader("Content-type", "application/json");
+		xmlhttp1.send("");
+
+        xmlhttp2.open("POST", SERVERADDRESS2, true);
+		xmlhttp2.setRequestHeader("Content-type", "application/json");
+		xmlhttp2.send("");
+        
+	}
+
+	getData();
+    
+}
+
+
+// when a player scores, we reset the ball
+function resetBall(){
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+    ball.velocityX = -ball.velocityX;
+    ball.speed = 7;
+}
+
+// draw the net
+function drawNet(){
+    for(let i = 0; i <= canvas.height; i+=15){
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+    }
+}
+
+// draw text
+function drawText(text,x,y){
+    ctx.fillStyle = "#FFF";
+    ctx.font = "75px fantasy";
+    ctx.fillText(text, x, y);
+}
+
+// collision detection
+function collision(b,p){
+    p.top = p.y;
+    p.bottom = p.y + p.height;
+    p.left = p.x;
+    p.right = p.x + p.width;
+    
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x + b.radius;
+    
+    return p.left < b.right && p.top < b.bottom && p.right > b.left && p.bottom > b.top;
+}
+
+// update function, the function that does all calculations
+function update(){
+    
+    updatePlayerPosition();
+
+    // change the score of players, if the ball goes to the left "ball.x<0" computer win, else if "ball.x > canvas.width" the user win
+    if( ball.x - ball.radius < 0 ){
+        player2.score++;
+        player2Score.play();
+        resetBall();
+    }else if( ball.x + ball.radius > canvas.width){
+        player1.score++;
+        player1Score.play();
+        resetBall();
+    }
+    
+    // the ball has a velocity
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    
+    // computer plays for itself, and we must be able to beat it
+    // simple AI
+    //com.y += ((ball.y - (com.y + com.height/2)))*0.1;
+    
+    // when the ball collides with bottom and top walls we inverse the y velocity.
+    if(ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height){
+        ball.velocityY = -ball.velocityY;
+        wall.play();
+    }
+    
+    // we check if the paddle hit the user or the com paddle
+    let player = (ball.x + ball.radius < canvas.width/2) ? player1 : player2;
+    
+    // if the ball hits a paddle
+    if(collision(ball,player)){
+        // play sound
+        hit.play();
+        // we check where the ball hits the paddle
+        let collidePoint = (ball.y - (player.y + player.height/2));
+        // normalize the value of collidePoint, we need to get numbers between -1 and 1.
+        // -player.height/2 < collide Point < player.height/2
+        collidePoint = collidePoint / (player.height/2);
+        
+        // when the ball hits the top of a paddle we want the ball, to take a -45degees angle
+        // when the ball hits the center of the paddle we want the ball to take a 0degrees angle
+        // when the ball hits the bottom of the paddle we want the ball to take a 45degrees
+        // Math.PI/4 = 45degrees
+        let angleRad = (Math.PI/4) * collidePoint;
+        
+        // change the X and Y velocity direction
+        let direction = (ball.x + ball.radius < canvas.width/2) ? 1 : -1;
+        ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+        ball.velocityY = ball.speed * Math.sin(angleRad);
+        
+        // speed up the ball everytime a paddle hits it.
+        ball.speed += 0.1;
+    }
+}
+
+// render function, the function that does al the drawing
+function render(){
+    
+    // clear the canvas
+    drawRect(0, 0, canvas.width, canvas.height, "#000");
+    
+    // draw the user score to the left
+    drawText(player1.score,canvas.width/4,canvas.height/5);
+    
+    // draw the COM score to the right
+    drawText(player2.score,3*canvas.width/4,canvas.height/5);
+    
+    // draw the net
+    drawNet();
+    
+    // draw player 1's paddle
+    drawRect(player1.x, player1.y, player1.width, player1.height, player1.color);
+
+    // draw player 2's paddle
+    drawRect(player2.x, player2.y, player2.width, player2.height, player2.color);
+    
+    // draw the COM's paddle
+    //drawRect(com.x, com.y, com.width, com.height, com.color);
+    
+    // draw the ball
+    drawArc(ball.x, ball.y, ball.radius, ball.color);
+}
+function game(){
+    update();
+    render();
+}
+// number of frames per second
+let framePerSecond = 50;
+
+//call the game function 50 times every 1 Sec
+let loop = setInterval(game,1000/framePerSecond);
+
